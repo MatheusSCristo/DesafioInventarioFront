3c7afbfa9c249623fc50ddd57952e02e52c408fc
Matheus <cristo.matheus@outlook.com>
2024-11-12 21:22:46 -0300

    feat: Add PurchaseOverviewDashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import PurchaseOverviewDashboard from "./components/PurchaseOverviewDashboard";
 import SalesOverviewDashboard from "./components/SalesOverviewDashboard";
 
 function App() {
   return (
     <main className="flex gap-5 bg-gray-50 p-10 min-h-screen">
-      <div className="w-1/2 flex flex-col">
+      <div className="w-1/2 flex flex-col gap-5">
         <SalesOverviewDashboard/>
-
+        <div className="flex gap-5">
+          <PurchaseOverviewDashboard/>
+        </div>
       </div>
 
     </main>
